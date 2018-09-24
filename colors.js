c1082b88314876c2838ddfec7d2268e888b02904
pdes-studio <paulo-mega@hotmail.com>
2018-09-24 12:09:27 -0300

    5

--- colors.js
+++ colors.js
@@ -15,39 +15,41 @@ function colorJS(){
         let letterElementf = document.querySelector('.firstLetter span').innerHTML = firstLetter;
         let letterElementl = document.querySelector('.lastLetter span').innerHTML = btnValue[lastLetter];
         let lengthElement = document.querySelector('.length span').innerHTML = wordLength;
-        let notification = document.querySelector('p.notification').innerHTML;
-
         let bodyElement = document.querySelector('body').style.background = `${btnValue}`;
 
-        if(btnValue === 'azul'){
+        const notification = document.querySelector('p.notification');
+        let notText = notification.innerHTML = `Você digitou a cor: ${btnValue}`;
+        const notStyle = notification.style.textTransform = "lowercase";
+
+        if(btnValue === 'azul' || btnValue === 'Azul'){
             bodyElement = document.querySelector('body').style.background = `#0652DD`;
         }
 
-        if(btnValue === 'amarelo'){
+        if(btnValue === 'amarelo' || btnValue === 'Amarelo'){
             bodyElement = document.querySelector('body').style.background = `#FFC312`;
         }
 
-        if(btnValue === 'laranja'){
+        if(btnValue === 'laranja' || btnValue === 'Laranja'){
             bodyElement = document.querySelector('body').style.background = `#EE5A24`;
         }
 
-        if(btnValue === 'vermelho'){
+        if(btnValue === 'vermelho' || btnValue === 'Vermelho'){
             bodyElement = document.querySelector('body').style.background = `#ED4C67`;
         }
 
-        if(btnValue === 'verde'){
+        if(btnValue === 'verde' || btnValue === 'Verde'){
             bodyElement = document.querySelector('body').style.background = `#009432`;
         }
 
-        if(btnValue === 'roxo'){
+        if(btnValue === 'roxo' || btnValue === 'Roxo'){
             bodyElement = document.querySelector('body').style.background = `purple`;
         }
 
-        if(btnValue === 'marrom'){
+        if(btnValue === 'marrom' || btnValue === 'Marrom'){
             bodyElement = document.querySelector('body').style.background = `brown`;
         }
 
-        if(btnValue === 'preto'){
+        if(btnValue === 'preto' || btnValue === 'Preto'){
             bodyElement = document.querySelector('body').style.background = `#111`;
         }
 
@@ -55,7 +57,7 @@ function colorJS(){
             letterElementf = document.querySelector('.firstLetter span').innerHTML = 'x';
             letterElementl = document.querySelector('.lastLetter span').innerHTML = 'x';
             lengthElement = document.querySelector('.length span').innerHTML = 'x';
-            notification = document.querySelector('p.notification').innerHTML = 'Você não digitou nenhuma cor!';
+            notText = notification.innerHTML = 'Você não digitou nenhuma cor!';
         }
     }
 
@@ -66,3 +68,26 @@ function colorJS(){
 }
 
 colorJS();
+
+
+function tituloDigita (){
+
+    const h1 = document.querySelector('h1');
+    let textoArray = h1.innerHTML.split('');
+    let textoLenght = textoArray.length;
+
+    function typeWrite(elemento){
+        elemento.innerHTML = '';
+
+        textoArray.forEach( (letra, index) => {
+            setTimeout(function() {
+                elemento.innerHTML += letra;
+            }, 150 * index);
+        });
+    }
+
+typeWrite(h1);
+
+}
+
+tituloDigita();
